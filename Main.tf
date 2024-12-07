485d4837a5bdfc7fe2fc57e6bfd5712c31daf940
Hamras Hilar A <hamrashilar@gmail.com>
2024-12-07 13:31:02 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -118,27 +118,13 @@ resource "aws_security_group" "k8s_sg" {
   description = "Allow necessary traffic for Kubernetes cluster"
   vpc_id      = aws_vpc.k8s_vpc.id
 
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  
 
   ingress {
-    description = "Allow Kubernetes API Server traffic"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow pod communication"
+    description = "Allow all outbound communication"
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
